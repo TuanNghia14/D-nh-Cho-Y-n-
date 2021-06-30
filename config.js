a86cd3074641b1f8ab71883b09b9c56c632847e6
TuanNghia14 <86749369+TuanNghia14@users.noreply.github.com>
2021-07-01 06:34:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Xin chào chú heo con của Tuấn Nghĩa!',
+    introDesc: `Anh Yêu Em đến điên đầu mà anh lại thấy như thế nó rất ngầu`,
+    btnIntro: 'Ấn vào là Yêu Mình đấy nhá',
+    title: 'Giữa mình và đồ ăn yến chọn cái nào',
+    desc: 'Yến chọn thế nào mình vẫn yêu Yến ',
+    btnYes: 'Tất nhiên là Nghĩa rồi <3',
+    btnNo: 'Chọn cả 2 rồi',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Yến lại yêu mình ',
+    btnReply: 'Gửi cho Nghĩa nè <3',
+    reply: 'Tại Nghĩa đẹp trai đó mà',
+    mess: 'Mà Yến này ',
+    messDesc: 'Yến sẽ mãi mãi là heo con của mình chứ ?.',
+    btnAccept: 'Tất nhiên rồi <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
