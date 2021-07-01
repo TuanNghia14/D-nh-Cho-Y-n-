0fe2ef3bbb122b82d5182ef89c0b389cceaf110a
TuanNghia14 <86749369+TuanNghia14@users.noreply.github.com>
2021-07-01 17:01:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
-const CONFIG = {
+    const CONFIG = {
     introTitle: 'Xin chào chú heo con của Tuấn Nghĩa!',
     introDesc: `Anh Yêu Em đến điên đầu mà anh lại thấy như thế nó rất ngầu`,
     btnIntro: 'Ấn vào là Yêu Mình đấy nhá',
     title: 'Giữa mình và đồ ăn yến chọn cái nào',
     desc: 'Yến chọn thế nào mình vẫn yêu Yến ',
     btnYes: 'Tất nhiên là Nghĩa rồi <3',
-    btnNo: 'Chọn cả 2 rồi',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Yến lại yêu mình ',
+    btnNo: 'Chọn cả 2',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Yến lại yêu mình',
     btnReply: 'Gửi cho Nghĩa nè <3',
     reply: 'Tại Nghĩa đẹp trai đó mà',
     mess: 'Mà Yến này',
     messDesc: 'Yến sẽ mãi mãi là heo con của mình chứ ?.',
     btnAccept: 'Tất nhiên rồi <3',
- 
